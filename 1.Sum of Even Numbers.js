feeef5c57b1b5dce4dc5457ba48251ce2f8b2ed7
alokksiet <38726052+alokksiet@users.noreply.github.com>
2023-08-14 14:47:52 +0530

    Update 1.Sum of Even Numbers.js

--- 1.Sum of Even Numbers.js
+++ 1.Sum of Even Numbers.js
@@ -1,4 +1,4 @@
-
+//Given an array of integers, your task is to find the sum of all even numbers present in the array.
 function sumOfEvenNumbers(arr){
     var sum = 0;
     arr.forEach(element => {
