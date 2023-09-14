04053747398d5eaff2cf1071928d784188b64b73
alokksiet <38726052+alokksiet@users.noreply.github.com>
2023-09-14 23:34:50 +0530

    Create 6.Finding Square Roots.js

---
+++ 6.Finding Square Roots.js
@@ -0,0 +1,5 @@
+//You are given a positive integer N. Your task is to find the square root of N rounded down to the nearest integer.
+function findSquareRoot(n){
+    var num = Math.sqrt(n);
+    return Math.floor(num);
+}
